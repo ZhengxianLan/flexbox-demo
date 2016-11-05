9ff62569011114db9ed5337f9b8820cb9ecd3832
lan <lanzhengxian@gmail.com>
2016-11-05 22:23:33 +0800

    Use font-awesome

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -3,9 +3,15 @@ var sass = require('gulp-sass') ;
 var autoprefixer = require('gulp-autoprefixer');
 var watch = require('gulp-watch');
 
+// 处理异常，防止在编译sass出错后，gulp直接退出
+function handle_error(err) {
+  console.error(err.toString());
+  this.emit('end');
+}
+
 gulp.task('sass',function () {
   gulp.src('styles/main.scss')
-  .pipe(sass())
+  .pipe(sass()).on('error',handle_error)
   .pipe(autoprefixer())
   .pipe(gulp.dest('../styles'));
 });
