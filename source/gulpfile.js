4f11835e5c6c4a90cce964f067ada89baade5a5b
lan <lanzhengxian@gmail.com>
2016-11-05 23:22:42 +0800

    use gulp wrap

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -2,12 +2,20 @@ var gulp = require('gulp');
 var sass = require('gulp-sass') ;
 var autoprefixer = require('gulp-autoprefixer');
 var watch = require('gulp-watch');
+var wrap = require('gulp-wrap');
+
 
 // 处理异常，防止在编译sass出错后，gulp直接退出
 function handle_error(err) {
   console.error(err.toString());
   this.emit('end');
 }
+
+gulp.task('build',function () {
+  gulp.src('pages/*.html')
+    .pipe(wrap({src:'layout/default.html'}))
+    .pipe(gulp.dest('..'));
+});
 
 gulp.task('sass',function () {
   gulp.src('styles/main.scss')
@@ -16,16 +24,14 @@ gulp.task('sass',function () {
   .pipe(gulp.dest('../styles'));
 });
 
-gulp.task('cp',function () {
-  gulp.src('./index.html')
-    .pipe(gulp.dest('../'));
-});
 
 gulp.task('watch',function(){
-  gulp.watch([ '*.html' ],['cp']);
+  // gulp.watch([ '**/*.html' ],['build']); // 使用 **/* 会heapout，这个版本的问题π
+  gulp.watch(['pages/*.html','layout/*.html'], ['build']);
   // 必须具体到文件，而不能使用文件夹
   gulp.watch(['styles/*'],['sass']);
+  console.log('watching')
 });
 
 
-gulp.task('default',['sass','cp','watch']);
+gulp.task('default',['sass','build','watch']);
